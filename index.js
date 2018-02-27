c85a81d1fe79ac5e194bfc6a6c3a8aa4522fe451
HM-demo <36228262+HM-demo@users.noreply.github.com>
2018-02-27 20:35:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('canary deployment with istio-v4!')
+  response.send('canary deployment with istio-v5!')
 })
 
 app.listen(app.get('port'), function() {
